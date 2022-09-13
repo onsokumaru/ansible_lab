70a73b0f62e71c3f4fc600f92596d34cee1a00be
onsokumaru <11082686+onsokumaru@users.noreply.github.com>
2022-09-12 21:20:30 -0400

    initial commit

---
+++ ubuntu/control/start_ansible.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# ==========================================
+# File: start_ansible.sh 
+# Author: onsokumaru
+# Date: 2022-09-08
+# Description: script to kick off ansible
+# docker container
+# ==========================================
+
+
+
+
